62568d9fef1b816e9f49456829b9882242646af4
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-18 16:54:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,5 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName
-  return playist
+  return delete playlist.artistName
 }
