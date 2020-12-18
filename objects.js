bca3880f50b38ae1ab449b30b135cb1b740cdbb8
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-18 17:05:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,5 +7,5 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  return delete ({'Slowdive': 'Alison'})
+  return delete playlist.artistName
 }
